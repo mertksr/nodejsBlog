cc7297600d536d12b734563658fcba79ea08fd9c
mert <mertksr@users.noreply.github.com>
2023-03-11 23:46:45 +0300

    still in developing

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const ejs = require('ejs');
+const Post = require('./models/Post')
+
+
+const app = express();
+mongoose.connect('mongodb://localhost/pcat-test-db');
+
 /***PUBLİC İÇERİSİNDE STATİK DOSYALAR VAR DİYOR***/
 app.use(express.static('public'));
 /*******/
-/*** TEMPLATE ENGİNE'I EJS OLARAK AYARLADIK ***/
+
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+    /*** TEMPLATE ENGİNE'I EJS OLARAK AYARLADIK ***/
 app.set("view engine", "ejs");
 /*******/
 app.get("/", (req, res, ) => {
@@ -19,7 +29,10 @@ app.get("/add-post", (req, res, ) => {
 app.get("/post", (req, res, ) => {
     res.render('post')
 });
-
+app.post('/posts', async(req, res) => {
+    await Post.create(req.body)
+    res.redirect('/')
+})
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server ${port} portunda dinleniyor`);
---
+++ models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+
+const PostSchema = new Schema({
+    postName: String,
+    postDetail: String,
+})
+
+const Post = mongoose.model('posts', PostSchema);
+module.exports = Post
